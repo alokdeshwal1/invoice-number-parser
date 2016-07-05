59689fd7123ed6f0c8e279321f31c0a670774414
Nir Peled <nir@wisdo.com>
2016-07-06 00:04:42 +0300

    [FEAT] add parse number and parse digit helpers

--- app/components/homepage/homepage.js
+++ app/components/homepage/homepage.js
@@ -6,34 +6,66 @@ import helpers from '../../helpers/helpers.js';
 
 var Homepage = React.createClass({
 
+    getInitialState: function() {
+
+        return {
+            file: null
+        }
+
+    },
+
     readFile: function(e) {
 
         helpers.logger('[Homepage] readFile');
 
-        var props = this.props,
-            state = this.state,
-            reader = new FileReader(),
+        var reader = new FileReader(),
             file = e.target.files[0],
-            allowedFileTypes = ['text/plain'];
+            allowedFileTypes = ['text/plain'],
+            numbers = [];
 
         if (file && !_.includes(allowedFileTypes, file.type)) {
             console.log('Please choose a valid image file');
             return;
         }
 
-        reader.onload = function() {
-            console.log(reader.result);
-        };
-
         if (file) {
-            reader.readAsBinaryString(file);
+            reader.readAsText(file);
         }
+
+        reader.onload = function() {
+
+            var text = reader.result,
+                lines = text.split('\n'),
+                length = lines.length,
+                amount = _.floor(length / 4);
+
+            _.times(amount, function(index) {
+                var position = index * 4;
+                numbers.push(helpers.parseNumber(lines[position].substr(0, 27) + lines[position + 1].substr(0, 27) + lines[position + 2].substr(0, 27)));
+            });
+
+            console.log(numbers);
+
+            console.log('**********************');
+
+            _.each(numbers, (number) => {
+               console.log(number + '\n');
+            });
+
+            console.log('**********************');
+
+        };
 
     },
 
     render: function () {
 
         helpers.logger('[Homepage] render');
+
+        var props = this.props,
+            state = this.state;
+
+        console.log(state);
 
         return (
             <section className="box-row box-homepage">
--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,6 +1,8 @@
 import logger from './logger.js';
 import safeStringify from './safe-stringify.js';
 import localStorage from './local-storage.js';
+import parseNumber from './parse-number.js';
+import parseDigit from './parse-digit.js';
 
 class Helpers {
 
@@ -8,6 +10,8 @@ class Helpers {
         this.logger = logger;
         this.safeStringify = safeStringify;
         this.localStorage = localStorage;
+        this.parseNumber = parseNumber;
+        this.parseDigit = parseDigit;
     }
 }
 
